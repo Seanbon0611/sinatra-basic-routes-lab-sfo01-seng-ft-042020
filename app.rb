de0d0374b9e34130ee326539b17b8c32f97c7874
Seanbon0611 <seand0611@gmail.com>
2020-05-11 14:53:26 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Sean"
+  end
+
+  get '/hometown' do
+    "My hometown is Daly City, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Rick Astley - Never gonna give you up"
+  end
 end
